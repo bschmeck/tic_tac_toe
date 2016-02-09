d1c9dbcc6b055c3a0358cba5252547f3db5d70a3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-02-09 12:58:31 -0600

    Organize winner arg list to show winning positions

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,14 +1,30 @@
 defmodule TicTacToe do
   def start, do: {:blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank}
 
-  def winner({x, x, x, _, _, _, _, _, _}) when x != :blank, do: x
-  def winner({_, _, _, x, x, x, _, _, _}) when x != :blank, do: x
-  def winner({_, _, _, _, _, _, x, x, x}) when x != :blank, do: x
-  def winner({x, _, _, x, _, _, x, _, _}) when x != :blank, do: x
-  def winner({_, x, _, _, x, _, _, x, _}) when x != :blank, do: x
-  def winner({_, _, x, _, _, x, _, _, x}) when x != :blank, do: x
-  def winner({x, _, _, _, x, _, _, _, x}) when x != :blank, do: x
-  def winner({_, _, x, _, x, _, x, _, _}) when x != :blank, do: x
+  def winner({x, x, x,
+              _, _, _,
+              _, _, _}) when x != :blank, do: x
+  def winner({_, _, _,
+              x, x, x,
+              _, _, _}) when x != :blank, do: x
+  def winner({_, _, _,
+              _, _, _,
+              x, x, x}) when x != :blank, do: x
+  def winner({x, _, _,
+              x, _, _,
+              x, _, _}) when x != :blank, do: x
+  def winner({_, x, _,
+              _, x, _,
+              _, x, _}) when x != :blank, do: x
+  def winner({_, _, x,
+              _, _, x,
+              _, _, x}) when x != :blank, do: x
+  def winner({x, _, _,
+              _, x, _,
+              _, _, x}) when x != :blank, do: x
+  def winner({_, _, x,
+              _, x, _,
+              x, _, _}) when x != :blank, do: x
   def winner(_board), do: nil
 
   def cats_game?(board) do
