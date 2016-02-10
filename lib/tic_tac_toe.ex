cdb4843cce47e861913c4abb54ad7c6037c0e211
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-02-09 20:59:42 -0600

    Refactor to hold game state in a Struct

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,72 +1,75 @@
 defmodule TicTacToe do
-  def start, do: {:blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank}
+  defmodule Game do
+    defstruct board: {:blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank, :blank},
+              dimension: 3
+  end
 
-  def winner({x, x, x,
-              _, _, _,
-              _, _, _}) when x != :blank, do: x
-  def winner({_, _, _,
-              x, x, x,
-              _, _, _}) when x != :blank, do: x
-  def winner({_, _, _,
-              _, _, _,
-              x, x, x}) when x != :blank, do: x
-  def winner({x, _, _,
-              x, _, _,
-              x, _, _}) when x != :blank, do: x
-  def winner({_, x, _,
-              _, x, _,
-              _, x, _}) when x != :blank, do: x
-  def winner({_, _, x,
-              _, _, x,
-              _, _, x}) when x != :blank, do: x
-  def winner({x, _, _,
-              _, x, _,
-              _, _, x}) when x != :blank, do: x
-  def winner({_, _, x,
-              _, x, _,
-              x, _, _}) when x != :blank, do: x
-  def winner(_board), do: nil
+  def start, do: %Game{}
 
-  def won?(board), do: winner(board) != nil
+  def winner(%Game{board: {x, x, x,
+                           _, _, _,
+                           _, _, _}}) when x != :blank, do: x
+  def winner(%Game{board: {_, _, _,
+                           x, x, x,
+                           _, _, _}}) when x != :blank, do: x
+  def winner(%Game{board: {_, _, _,
+                           _, _, _,
+                           x, x, x}}) when x != :blank, do: x
+  def winner(%Game{board: {x, _, _,
+                           x, _, _,
+                           x, _, _}}) when x != :blank, do: x
+  def winner(%Game{board: {_, x, _,
+                           _, x, _,
+                           _, x, _}}) when x != :blank, do: x
+  def winner(%Game{board: {_, _, x,
+                           _, _, x,
+                           _, _, x}}) when x != :blank, do: x
+  def winner(%Game{board: {x, _, _,
+                           _, x, _,
+                           _, _, x}}) when x != :blank, do: x
+  def winner(%Game{board: {_, _, x,
+                           _, x, _,
+                           x, _, _}}) when x != :blank, do: x
+  def winner(%Game{}), do: nil
+
+  def won?(game), do: winner(game) != nil
   
-  def cats_game?(board) do
-    if winner(board) do
+  def cats_game?(game) do
+    if winner(game) do
       false
     else
-      board |> locations |> Enum.all?(fn(loc) -> !blank?(board, loc) end)
+      game |> locations |> Enum.all?(fn(loc) -> !blank?(game, loc) end)
     end
   end
 
-  def game_over?(board) do
-    won?(board) or cats_game?(board)
+  def game_over?(game) do
+    won?(game) or cats_game?(game)
   end
 
-  def blank?(board, {x, y}) do
-    get_mark_at(board, {x, y}) == :blank
+  def blank?(game, {x, y}) do
+    get_mark_at(game, {x, y}) == :blank
   end
 
-  def claim(board, {x, y}, mark) do
-    if blank?(board, {x, y}) do
-      {:ok, set_mark_at(board, {x, y}, mark)}
+  def claim(game, {x, y}, mark) do
+    if blank?(game, {x, y}) do
+      {:ok, set_mark_at(game, {x, y}, mark)}
     else
-      {:error, board}
+      {:error, game}
     end
   end
 
-  def locations(board) do
-    range = 0..(dimension(board) - 1)
+  def locations(game) do
+    range = 0..(game.dimension - 1)
     Enum.flat_map(range, fn(x) -> Enum.map(range, fn(y) -> {x, y} end) end)
   end
 
-  defp dimension(board), do: board |> tuple_size |> :math.sqrt |> trunc
-
-  defp get_mark_at(board, {x, y}) do
-    index = x * dimension(board) + y
-    elem(board, index)
+  defp get_mark_at(game, {x, y}) do
+    index = x * game.dimension + y
+    elem(game.board, index)
   end
 
-  defp set_mark_at(board, {x, y}, mark) do
-    index = x * dimension(board) + y
-    put_elem(board, index, mark)
+  defp set_mark_at(game, {x, y}, mark) do
+    index = x * game.dimension + y
+    %Game{game | board: put_elem(game.board, index, mark) }
   end
 end
--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -7,44 +7,44 @@ defmodule TicTacToeTest do
   end
 
   test "all spots are blank on a new board" do
-    board = TicTacToe.start
-    assert board |>
+    game = TicTacToe.start
+    assert game |>
       TicTacToe.locations |>
-      Enum.all?(fn(loc) -> TicTacToe.blank? board, loc end)
+      Enum.all?(fn(loc) -> TicTacToe.blank? game, loc end)
   end
 
   test "after claiming, a spot is not blank" do
     loc = { 1, 1 }
-    {:ok, board} = TicTacToe.start |> TicTacToe.claim(loc, :x)
-    refute TicTacToe.blank?(board, loc)
+    {:ok, game} = TicTacToe.start |> TicTacToe.claim(loc, :x)
+    refute TicTacToe.blank?(game, loc)
   end
 
   test "a previously claimed spot cannot be claimed" do
     loc = {1, 0}
-    {:ok, board} = TicTacToe.start |> TicTacToe.claim(loc, :x)
-    assert {:error, board} == TicTacToe.claim(board, loc, :o)
+    {:ok, game} = TicTacToe.start |> TicTacToe.claim(loc, :x)
+    assert {:error, game} == TicTacToe.claim(game, loc, :o)
   end
 
   test "a board with blank locations is not a cat's game" do
-    board = {:x, :x, :o, :o, :x, :o, :x, :o, :blank}
-    refute TicTacToe.cats_game?(board)
+    game = %TicTacToe.Game{board: {:x, :x, :o, :o, :x, :o, :x, :o, :blank} }
+    refute TicTacToe.cats_game?(game)
   end
 
   test "a winning board is not a cat's game" do
-    board = {:x, :x, :o, :o, :x, :o, :x, :o, :o}
-    assert TicTacToe.winner(board)
-    refute TicTacToe.cats_game?(board)
+    game = %TicTacToe.Game{board: {:x, :x, :o, :o, :x, :o, :x, :o, :o} }
+    assert TicTacToe.winner(game)
+    refute TicTacToe.cats_game?(game)
   end
 
   test "a cat's game is a cat's game" do
-    board = {:x, :x, :o, :o, :o, :x, :x, :o, :x}
-    refute TicTacToe.winner(board)
-    assert TicTacToe.cats_game?(board)
+    game = %TicTacToe.Game{board: {:x, :x, :o, :o, :o, :x, :x, :o, :x} }
+    refute TicTacToe.winner(game)
+    assert TicTacToe.cats_game?(game)
   end
 
   test "cat's games are over" do
-    board = {:x, :x, :o, :o, :o, :x, :x, :o, :x}
-    assert TicTacToe.cats_game?(board)
-    assert TicTacToe.game_over?(board)
+    game = %TicTacToe.Game{ board: {:x, :x, :o, :o, :o, :x, :x, :o, :x} }
+    assert TicTacToe.cats_game?(game)
+    assert TicTacToe.game_over?(game)
   end
 end
