18f4af608dc16cc8c1f8f89e7d77e3831a3369aa
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-03-06 21:54:50 -0600

    Run a TicTacToe game in the console

--- lib/tic_tac_toe/cli.ex
+++ lib/tic_tac_toe/cli.ex
@@ -1,8 +1,31 @@
 defmodule TicTacToe.CLI do
-  alias TicTacToe.{CLI, Game}
+  alias TicTacToe.{Board, CLI, Game}
+
+  def loop(game) do
+    print_board(game)
+    move = get_move
+    {:ok, game} = Game.claim game, move
+    if Game.game_over? game do
+      print_board(game)
+      IO.puts "GAME OVER"
+    else
+      loop(game)
+    end
+  end
+
+  def get_move do
+    [x, y] = IO.gets("Move: ")
+    |> String.rstrip
+    |> String.split
+    |> Enum.map(fn(s) ->
+      {i, _} = Integer.parse(s)
+      i - 1
+    end)
+    {x, y}
+  end
 
   def print_board(game) do
-    IO.puts(Game.rows(game) |> Enum.map(&(row_to_s(&1))) |> Enum.join(row_separator))
+    IO.puts(Board.rows(game.board) |> Enum.map(&(row_to_s(&1))) |> Enum.join(row_separator))
   end
 
   defp row_to_s(row) do
@@ -12,7 +35,7 @@ defmodule TicTacToe.CLI do
   defp row_separator, do: "\n---|---|---\n"
   defp cell_separator, do: "|"
 
-  defp cell_to_s(:blank), do: "   "
   defp cell_to_s(:x), do: " X "
   defp cell_to_s(:o), do: " O "
+  defp cell_to_s(_), do: "   "
 end
